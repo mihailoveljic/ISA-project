0a63f59219422d5bf9481029904441a7afccafbf
Veljic <veljic.ra31.2019@uns.ac.rs>
2022-10-22 16:30:19 +0200

    init

---
+++ isa-backend/src/main/java/eu/DreamTeam/isabackend/IsaBackendApplication.java
@@ -0,0 +1,13 @@
+package eu.DreamTeam.isabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsaBackendApplication.class, args);
+	}
+
+}
