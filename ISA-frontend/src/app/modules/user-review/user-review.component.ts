402d2404e56b6137375a81fd99baf5d56136abfa
Milena Jelic <jelic.ra23.2019@uns.ac.rs>
2022-11-15 21:54:14 +0100

    Search of users

--- ISA-frontend/src/app/modules/user-review/user-review.component.ts
+++ ISA-frontend/src/app/modules/user-review/user-review.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from "@angular/material/table";
+import { AdminNavbarComponent } from '../navbar/components/admin-navbar/admin-navbar.component';
 import { PersonInfo } from '../staff-info/model/PersonInfo';
 import { UserReviewService } from './service/user-review.service';
 @Component({
@@ -11,15 +12,26 @@ export class UserReviewComponent implements OnInit {
   public dataSource = new MatTableDataSource<PersonInfo>();
   public displayedColumns = ['name', 'surname', 'jmbg'];
   public users: PersonInfo[] = [];
+  public users_filtered: PersonInfo[] = [];
   public user: PersonInfo | undefined = undefined;
+  public name: string = ''
+  public surname: string = ''
 
   constructor(private userReviewService: UserReviewService) { }
 
   ngOnInit(): void {
     this.userReviewService.getAllPersons().subscribe(res => {
       this.users = res;
-      this.dataSource.data = this.users;
+      this.users_filtered = res;
+      this.dataSource.data = this.users_filtered;
     })
+  }
+
+  search(){
+    this.users_filtered = this.users.filter(u => {return (u.name.toLowerCase().includes(this.name.toLowerCase()) &&
+                   u.surname.toLowerCase().includes(this.surname.toLowerCase())); } )
+    //this.treninziSaDatumom.filter(o => o.treninzi.price >= minCena && o.treninzi.price <= maxCena);
+    this.dataSource.data = this.users_filtered 
   }
 
 }
