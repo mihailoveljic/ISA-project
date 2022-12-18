3f68c4abb648719bee5faf95b490e8dc0f7a4233
Milena Jelic <jelic.ra23.2019@uns.ac.rs>
2022-12-18 19:46:25 +0100

    Calendar implemented

---
+++ ISA-frontend/src/app/modules/calendar/calendar.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { FlatpickrModule } from 'angularx-flatpickr';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { CalendarComponent } from './calendar/calendar.component';
+import { CalendarRoutingModule } from './calendar-routing.module';
+
+@NgModule({
+  imports: [
+    CalendarRoutingModule,
+    CommonModule,
+    FormsModule,
+    NgbModalModule,
+    FlatpickrModule.forRoot(),
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
+  ],
+  declarations: [CalendarComponent],
+  exports: [CalendarComponent],
+})
+export class Calendar1Module {}
