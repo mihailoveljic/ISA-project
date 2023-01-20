ec3d04dce4a7d1ebd2880c72bdc69314be4b443e
Veljic <veljic.ra31.2019@uns.ac.rs>
2023-01-20 23:07:20 +0100

    eureka service discovery and api gateway added

---
+++ ISA-eureka-discovery/isa-eureka-server/src/test/java/isa/isaeurekaserver/IsaEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package isa.isaeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsaEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- isa-backend/src/main/java/eu/dreamTeam/isabackend/config/CorsConfiguration.java
+++ isa-backend/src/main/java/eu/dreamTeam/isabackend/config/CorsConfiguration.java
@@ -1,17 +1,20 @@
-package eu.dreamTeam.isabackend.config;
+//package eu.dreamTeam.isabackend.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.annotation.Order;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//@EnableWebMvc
+//public class CorsConfiguration implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/api/**").allowedOrigins("http://localhost:4200").allowedMethods("*").allowedHeaders("*");
+//    }
+//}
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-@EnableWebMvc
-public class CorsConfiguration implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
-    }
-}
+//POTENCIJALNO NE TREBA JER SADA API GATEWAY KONTROLISE CORS
+//ZAPRAVO NE SME DA POSTOJI DODATNA KONFIGURACIJA ZA CORS KADA API-GATEWAY IMA SVOJU!!!
