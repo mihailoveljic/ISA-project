d35d350865f69f5d9a9d1546264b36eacac25f80
Miloš Zeljko <miloszeljko00@gmail.com>
2023-01-25 12:37:02 +0100

    merge confilcts solved

--- isa-backend/src/main/java/eu/dreamTeam/isabackend/controller/DeliveryLocationController.java
+++ isa-backend/src/main/java/eu/dreamTeam/isabackend/controller/DeliveryLocationController.java
@@ -14,7 +14,7 @@ import org.springframework.web.client.RestTemplate;
 import java.util.ArrayList;
 import java.util.List;
 
-@CrossOrigin
+
 @RestController
 @RequestMapping("/api/delivery-location")
 public class DeliveryLocationController {
